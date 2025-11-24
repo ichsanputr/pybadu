a96aa160b71b3d014113b54a93c8e21e52e0535c
ichsanputr <ngekode24@gmail.com>
2025-11-24 10:05:23 +0700

    a

--- components/ui/Dialog.vue
+++ components/ui/Dialog.vue
@@ -2,7 +2,7 @@
   <!-- Backdrop -->
   <Teleport to="body">
     <div 
-      v-if="modelValue"
+      v-show="modelValue"
       class="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-md"
       @click="handleBackdropClick"
     >
